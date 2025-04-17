4d8a9c68db9ea83c3246eff3f7d0db5100ff0d65
Mukesh Kumar <mukeshkumar.in25@gmail.com>
2025-04-18 02:51:21 +0530

    ignore the env folder

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("Welcome to My Space")
+
+
+
+
+st.image("lord-hanuman-black-3840x2160-14679.png")
+
+col1 ,  col2 , col3 = st.columns(3)
+
+st.write("I surrender to Hanuman, the messenger of Lord Rama, who is swift as the mind and fast as the wind. He has mastered his senses and is the wisest among the wise. He is the son of the wind god and the chief among the Vanaras (monkey tribe).")
+# with col3:
+st.write("Rama Singhasan Singh, Maruti Veer Maharaj |SadaShiv, Sankat Mochan, Jai Hanuman Mooradhaar ||")
